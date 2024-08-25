2ae3f977f85c18e5e3e29651e32f39f700142274
MrRayliegh <edwardcole143@gmail.com>
2024-08-25 19:19:28 +0800

    Create pthtacv1.lua

---
+++ pthtacv1.lua
@@ -0,0 +1,17 @@
+return {
+    487237, -- OWNNER
+    729294, -- Ceyhan
+    731836, -- Ceyhan 
+    752406, -- Abii
+    45715,  -- RassW
+    808152, -- arobilajaa
+    828432, -- Celi
+    154017, -- mrandreson
+    589703, -- cha
+    349740, -- D3IVI5420TEST
+    19131,  -- Friendly Cactus
+    833847, -- JINXI
+    835190, -- JINXI2
+    555662, -- Boka
+    175865 -- DAY
+}
